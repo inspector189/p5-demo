1254fce799b6b29ce27848b394d4892d6b01ac15
Marta <tulciak189@gmail.com>
2024-03-15 21:49:41 +0100

    First Coding Challenge - Mathematical Rose Patterns

--- sketch.js
+++ sketch.js
@@ -1,7 +1,61 @@
+var d = 8;
+var n = 5;
+var sliderD;
+var sliderN;
+var sliderColor;
+var weightStrokeSlider;
+
 function setup() {
   createCanvas(400, 400);
+  colorMode(HSB, 360);
+
+  var controls = createDiv('');
+  controls.position(150, 520);
+
+  var dLabel = createP('D (denominator) value:');
+  dLabel.parent(controls);
+  sliderD = createSlider(1, 20, 1, 1);
+  sliderD.parent(controls);
+
+  var nLabel = createP('N (numerator) value:');
+  nLabel.parent(controls);
+  sliderN = createSlider(1, 20, 1, 1);
+  sliderN.parent(controls);
+
+  var colorLabel = createP('Vertex color:');
+  colorLabel.parent(controls);
+  sliderColor = createSlider(0, 360, 0, 0);
+  sliderColor.parent(controls);
+
+  var weightStrokeLabel = createP('Stroke weight:');
+  weightStrokeLabel.parent(controls);
+  weightStrokeSlider = createSlider(1, 55, 0, 0);
+  weightStrokeSlider.parent(controls);
+
+  sliderD.input(draw);
+  sliderN.input(draw);
+  sliderColor.input(draw);
+  weightStrokeSlider.input(draw);  
 }
 
 function draw() {
-  background(220);
+  d = sliderD.value();
+  n = sliderN.value();
+  var k = n/d;
+  var hue = sliderColor.value();
+  var strWeight = weightStrokeSlider.value();
+  background(60);
+  translate(width/2, height/2);
+  beginShape();
+  noFill();
+  strokeWeight(strWeight);
+  for(var a = 0; a < TWO_PI * d; a += 0.02) {
+    var r = 150 * cos(k * a);
+    var x = r * cos(a);
+    var y = r * sin(a);
+    stroke(hue, 255, 255);
+    strokeWeight(strWeight);
+    vertex(x,y);
+  }
+  endShape(CLOSE);
 }
